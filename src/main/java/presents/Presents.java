2e22dec6071016737a9810a6663f8e384c38a46a
Wouter <wouter@grijsloke.be>
2018-12-01 18:07:24 +0100

    Initial commit

---
+++ src/main/java/presents/Presents.java
@@ -0,0 +1,13 @@
+package presents;
+
+import net.minecraftforge.fml.common.Mod;
+
+@SuppressWarnings("WeakerAccess")
+@Mod(modid = Presents.MODID, name = Presents.MODNAME, version = Presents.MODVERSION, updateJSON = "https://github.com/ochotonida/presents/blob/master/update.json")
+public class Presents {
+
+    public static final String MODID = "presents";
+    public static final String MODNAME = "Presents";
+    public static final String MODVERSION = "1.12.2-0.0.1";
+
+}
